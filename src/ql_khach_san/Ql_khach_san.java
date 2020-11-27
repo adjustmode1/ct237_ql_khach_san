3a9bdc4f44e5b44877f4c79c20e1026bf6b012c6
= <=>
2020-11-27 18:10:24 +0700

    project

---
+++ src/ql_khach_san/Ql_khach_san.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ql_khach_san;
+
+/**
+ *
+ * @author DELL
+ */
+public class Ql_khach_san {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
